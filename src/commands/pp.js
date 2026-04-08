a16ee230e57817e00e5dab0a900bbc0cd28a1cf0
m4skedvanir <m4skedvanir@gmail.com>
2026-04-08 14:02:09 +0900

    v1.2.11

--- src/commands/pp.js
+++ src/commands/pp.js
@@ -136,7 +136,7 @@ async function ppCommand({ command, ack, respond }) {
       console.log('フォーカス終了処理開始');
       if (botToken) {
         console.log('channel_id:', timer.channel_id);
-        await sendMessage(botToken, timer.channel_id, `<@${command.user_id}> さん🍅料理をどうぞ！休憩${breakMin}分`);
+        await sendMessage(botToken, timer.user_id, `<@${command.user_id}> さん🍅料理をどうぞ！休憩${breakMin}分`);
       }
         // TODO: トマト料理をランダムに変える処理の追加
     },
@@ -147,7 +147,7 @@ async function ppCommand({ command, ack, respond }) {
         await restoreStatus(userToken, prevStatusText, prevStatusEmoji);
       }
       if (botToken) {
-        await sendMessage(botToken, timer.channel_id, `<@${command.user_id}> さん🍅Time終了です！`);
+        await sendMessage(botToken, timer.user_id, `<@${command.user_id}> さん🍅Time終了です！`);
       }
     },
   });
